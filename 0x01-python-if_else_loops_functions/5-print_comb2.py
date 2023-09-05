7e4a56defa64ad6dddb2efc45aace156c6f766c3
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-05 22:07:29 +0200

    for kayla

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-num = 0
+num = '0'
 for i in range(0, 100):
-    if (not (i == 99) and i < 10):
-        print("{}{}, ".format(num, i), end="")
-    elif (not (i == 99)):
-        print("{}, ".format(i), end="")
+    if (not (i == 99)):
+        print("{:02}, ".format(i), end="")
     else:
         print("{}".format(i))
