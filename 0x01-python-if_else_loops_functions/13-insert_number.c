2e7c052b73eef0025f8b60513a64ba80e9b73560
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-06 13:05:57 +0200

    changes to the insert function

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,13 +10,16 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *ptr, *new, *headptr;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 	{
 		(*new).n = number;
 		(*new).next = NULL;
+		*head = new;
 		return (new);
 	}
 	if ((**head).n > number)
