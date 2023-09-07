34fb0b2f21e5f0cf483723ff5ec709a06ca69564
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-08 00:46:17 +0200

    made changes to the 4-hidden task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for each in names:
-        if'__' not in each:
+        if '__' not in each:
             print("{}".format(each))
