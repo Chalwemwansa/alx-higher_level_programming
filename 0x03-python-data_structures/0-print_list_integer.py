2dda23e898e598bc94f0b5fb83752cb4350be684
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-10 19:38:42 +0200

    i do not know what they are looking for

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     max = len(my_list)
-
-    for i in range(0, max):
-        print("{0}".format(my_list[i]))
+    i = 0
+    while (1):
+        if i >= 0 and i < max:
+            print("{0}".format(my_list[i]))
+            i += 1
+        else:
+            break
