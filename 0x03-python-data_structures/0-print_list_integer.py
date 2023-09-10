23c8324f82285874ade16a1d9d2f4768e547fc91
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-10 19:08:50 +0200

    checking

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print("{}".format(my_list))
+    max = len(my_list)
+
+    for i in range(0, max):
+        p = my_list[i]
+        print("{}".format(p))
