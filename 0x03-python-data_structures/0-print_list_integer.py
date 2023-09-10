729db7728d22c32f632f65e38d5299b74f36fee0
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-10 19:01:44 +0200

    more changes made

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    max = len(my_list)
-    for i in range(0, max):
-        print("{}".format(my_list[i]))
+    print(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    max = len(my_list)
-    for i in range((max - 1), -1, -1):
-        print(f"{my_list[i])}")
+    print(my_list.reverse())
