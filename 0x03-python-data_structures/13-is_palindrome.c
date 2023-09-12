2e48c5e6832588366c908d2da626438fd44ad488
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-12 10:12:15 +0200

    another commit to the structures project, time to work on the last extra task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,25 +7,35 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr1 = *head;
+	listint_t *ptr1;
 	int *ptr, length, i = 0;
 	
 	if (head == NULL || *head == NULL)
 		return (1);
+	ptr1 = *head;
 	while (ptr1 != NULL)
 	{
 		i++;
 		ptr1 = (*ptr1).next;
 	}
-	ptr = malloc(sizeof(int) * i);
+	ptr = malloc(sizeof(int) * (i + 1));
+	if (ptr == NULL)
+		return (0);
+	ptr1 = *head;
 	length = i - 1;
-	while (ptr1 != NULL)
+	for (i = 0; i <= length; i++)
 	{
-		ptr += (*ptr1).n;
+		ptr[i] += (*ptr1).n;
 		ptr1 = (*ptr1).next;
 	}
-	for (i = 0; i <= (length / 2); i++)
+	for (i = 0; i <= length / 2; i++)
+	{
 		if (ptr[i] != ptr[(length - i)])
-				return (0);
+		{
+			free(ptr);
+			return (0);
+		}
+	}
+	free(ptr);
 	return (1);
 }
