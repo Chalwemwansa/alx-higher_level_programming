067b90656360c41155dc4451c93f445f6c8c9667
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-16 03:02:35 +0200

    last alteration to the best_score project

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return (None)
     if isinstance(a_dictionary, dict):
         score = next(iter(a_dictionary))
     else:
-        return (None)
-    if len(a_dictionary) == 0:
         return (None)
     for k, v in a_dictionary.items():
         if a_dictionary[score] < v:
