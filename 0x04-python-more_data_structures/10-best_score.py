1d8cef436ecfa557e262c235a64ea39ea481ee84
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-16 03:08:18 +0200

    another commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or (len(a_dictionary) == 0):
         return (None)
-    if isinstance(a_dictionary, dict):
-        score = next(iter(a_dictionary))
     else:
-        return (None)
+        score = next(iter(a_dictionary))
     for k, v in a_dictionary.items():
         if a_dictionary[score] < v:
             score = k
--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -4,5 +4,5 @@ a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
 best_key = best_score(a_dictionary)
 print("Best score: {}".format(best_key))
 mwalitumpa = 1
-best_key = best_score({})
+best_key = best_score(0)
 print("Best score: {}".format(best_key))
