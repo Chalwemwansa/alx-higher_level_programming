998a76514406a34d2b39ad62f7036dc6e6160cec
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-25 21:33:53 +0200

    another try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,6 @@ def safe_print_list(my_list=[], x=0):
             if (i == (x - 1)):
                 print()
                 return (i + 1)
-        except Exception:
+        except IndexError:
             print()
             return (i)
