25c4f6897d0d6e8c11c26cf0eaa43f3a915550ee
chalwemwansa <chalwemwansa23@gmail.com>
2023-09-26 21:53:21 +0200

    changes to python exceptions project

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     total = 0
-    
+
+    if (x <= 0):
+        return (total)
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,10 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except IndexError:
             print("out of range")
-            while (i < list_length):
-                new_list.append(0)
-                i += 1
-            break
+            new_list.append(0)
         finally:
             if (not (i == (list_length - 1))):
                 continue
