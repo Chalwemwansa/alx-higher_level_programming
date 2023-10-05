b68ff056888a4ddfdf860fe470a7718e3022084e
chalwemwansa <chalwemwansa23@gmail.com>
2023-10-05 03:43:25 +0200

    second commit

--- 0x08-python-more_classes/9-main.py
+++ 0x08-python-more_classes/9-main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
-
-my_square = Rectangle.square(5)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
+try:
+    my_square = Rectangle.square("12")
+    print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+    print(my_square)
+except Exception as e:
+    print("[{}] {}".format(e.class.name_, e))
 
