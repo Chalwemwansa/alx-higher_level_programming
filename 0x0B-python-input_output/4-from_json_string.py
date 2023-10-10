42d72a365b7513750233b08cae95b428525a6de3
chalwemwansa <chalwemwansa23@gmail.com>
2023-10-10 23:15:49 +0200

    another commit message

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").my_function.__doc__)'
+"""
+from json import load
+
+
+def from_json_string(my_str):
+    """converts a json string to an object
+    """
+
+    my_obj = load(my_str)
+    return (my_obj)
