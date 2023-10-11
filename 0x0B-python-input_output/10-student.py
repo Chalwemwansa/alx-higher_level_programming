f48b4e0c0397b3052cd309c7afcb3c4222d26526
chalwemwansa <chalwemwansa23@gmail.com>
2023-10-11 03:17:41 +0200

    probably last commit for the night

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
+class Student:
+    """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__
+    """
+
+    def __init__(self, first_name, last_name, age):
+        if not (isinstance(first_name, str)):
+            raise TypeError("first_name should be string")
+        if not (isinstance(last_name, str)):
+            raise TypeError("last_name should be string")
+        if not (isinstance(age, int)):
+            raise TypeError("Age must be an int")
+        if age < 0:
+            raise ValueError("age must be greater than or equal to 0")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            my_list = []
+            for i, v in self.__dict__.items():
+                if i in attrs:
+                    my_list.append((i, v))
+            return (dict(my_list))
+        else:
+            return (self.__dict__)
---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
+class Student:
+    """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__
+    """
+
+    def __init__(self, first_name, last_name, age):
+        if not (isinstance(first_name, str)) or not (isinstance(last_name, str)):
+            raise TypeError("first and last name should be string")
+        if not (isinstance(age, int)):
+            raise TypeError("Age must be an int")
+        if age < 0:
+            raise ValueError("age must be greater than or equal to 0")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return (self.__dict__)
