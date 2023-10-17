ac4a82144a2dafea59aa4ef9e8aaaa1a37a47963
chalwemwansa <chalwemwansa23@gmail.com>
2023-10-17 16:11:18 +0200

    more checking

--- 0x0C-python-almost_a_circle/tests/test_rect.py
+++ 0x0C-python-almost_a_circle/tests/test_rect.py
@@ -12,6 +12,7 @@ class Rect_test(unittest.TestCase):
     """
 
     obj1 = Rectangle(5, 5, 6, 10, 40)
+    obj2 = Rectangle(1, 2)
     def test_widthGetter(self):
         self.assertEqual(Rect_test.obj1.width, 5)
     def test_heightGetter(self):
@@ -23,29 +24,28 @@ class Rect_test(unittest.TestCase):
     def test_idGetter(self):
         self.assertEqual(Rect_test.obj1.id, 40)
     def test_twoargs(self):
-        obj1 = Rectangle(1, 2)
         def test_widthGetter(self):
-            self.assertEqual(Rect_test.obj1.width, 1)
+            self.assertEqual(Rect_test.obj2.width, 1)
         def test_heightGetter(self):
-            self.assertEqual(Rect_test.obj1.height, 2)
+            self.assertEqual(Rect_test.obj2.height, 2)
         def test_xGetter(self):
-            self.assertEqual(Rect_test.obj1.x, 0)
+            self.assertEqual(Rect_test.obj2.x, 0)
         def test_yGetter(self):
-            self.assertEqual(Rect_test.obj1.y, 0)
+            self.assertEqual(Rect_test.obj2.y, 0)
         def test_idGetter(self):
-            self.assertEqual(Rect_test.obj1.id, 1)
+            self.assertEqual(Rect_test.obj2.id, 1)
     def test_threeargs(self):
         obj1 = Rectangle(5, 5, 6)
         def test_heightGetter(self):
-            self.assertEqual(Rect_test.obj1.height, 5)
+            self.assertEqual(obj1.height, 5)
         def test_xGetter(self):
-            self.assertEqual(Rect_test.obj1.x, 6)
+            self.assertEqual(obj1.x, 6)
     def test_fourargs(self):
         obj1 = Rectangle(5, 5, 6, 10)
         def test_widthGetter(self):
-            self.assertEqual(Rect_test.obj1.width, 5)
+            self.assertEqual(obj1.width, 5)
         def test_yGetter(self):
-            self.assertEqual(Rect_test.obj1.y, 10)
+            self.assertEqual(obj1.y, 10)
     def test_errors(self):
         with self.assertRaises(TypeError):
             obj2 = Rectangle("1", 2)
