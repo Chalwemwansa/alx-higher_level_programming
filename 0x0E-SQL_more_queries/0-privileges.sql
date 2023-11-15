fb2e6fbb6db54c55ecc2d9235be6f178dd018e1d
chalwemwansa <chalwemwansa23@gmail.com>
2023-11-16 01:06:39 +0200

    checking

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- shows all privileges for some mysql users
+-- the users user_od_2 and user_od_1
+
+SHOW GRANTS FOR 'user_od_1'@'localhost';
+SHOW GRANTS FOR 'user_od_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+project on sql queries for the mysql database
