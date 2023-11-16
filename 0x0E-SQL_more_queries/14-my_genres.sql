16e33266d583772f1098933ceb3fcaa4fb4bbe9d
chalwemwansa <chalwemwansa23@gmail.com>
2023-11-16 18:30:45 +0200

    checking if 14 is ok

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- shows the genres for the title DEXTER
 -- demonstrating the use of the SELECT and the join operations
 SELECT tg.name
-FROM tv_shows ts JOIN tv_show_genres tsg JOIN tv_genres tg
+FROM tv_shows ts INNER JOIN tv_show_genres tsg INNER JOIN tv_genres tg
 ON tsg.show_id = ts.id AND tsg.genre_id = tg.id AND ts.title = 'Dexter';
