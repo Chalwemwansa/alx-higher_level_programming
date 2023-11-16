3f9b58774287da9863d0c125eb5cc65c76a1cc07
chalwemwansa <chalwemwansa23@gmail.com>
2023-11-16 18:32:58 +0200

    checking the same file 14 if all is ok

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,5 @@
 -- demonstrating the use of the SELECT and the join operations
 SELECT tg.name
 FROM tv_shows ts INNER JOIN tv_show_genres tsg INNER JOIN tv_genres tg
-ON tsg.show_id = ts.id AND tsg.genre_id = tg.id AND ts.title = 'Dexter';
+ON tsg.show_id = ts.id AND tsg.genre_id = tg.id AND ts.title = 'Dexter'
+ORDER BY tg.name;
