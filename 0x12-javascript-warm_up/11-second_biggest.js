ccbdb52bb2624237aace2aecc76ff0774c4dde00
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-11 23:30:38 +0200

    checking task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,9 @@
 
 let secondLargest = 0;
 let largest = 0;
+const len = process.argv.length;
 
-for (let i = 1; i < process.argv.length; i++) {
+for (let i = 1; i < len; i++) {
   const num = parseInt(process.argv[i], 10);
   if (largest < num) {
     secondLargest = largest;
