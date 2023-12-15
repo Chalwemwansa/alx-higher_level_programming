b0bfd12c181528a09f2ff0285a9ba86b4a716dc0
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 01:35:28 +0200

    worked on task2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,8 @@ def connect(usr, paswd, database):
 
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                 ORDER BY id ASC")
     try:
         resultSet = cur.fetchall()
         for tuple in resultSet:
