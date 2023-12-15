3af115870c1f7fa9616cedd4378edc63a6548c97
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 00:58:09 +0200

    checking task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     to just execute it without importing in their own moodule
     """
     con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                          passwd=argv[2], db=argv[3], charset="utf8")
+                          passwd=argv[2], db=argv[3])
     cur = con.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE('N%')")
+    cur.execute("SELECT * FROM states WHERE name LIKE(N%)")
     for tuple in cur.fetchall():
         print(tuple)
     cur.close()
