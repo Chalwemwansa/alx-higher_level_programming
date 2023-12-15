aac8ba476e0208016ccc0c2e31caf911fa6a4859
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 00:56:11 +0200

    checking if connected for task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""this python script is not a module
+    it is used to establish a connection and execute commands
+    from the python script itself
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """this is to ensure that the script only executes when
+    the script is not imported hence to use the script one has
+    to just execute it without importing in their own moodule
+    """
+    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3], charset="utf8")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE('N%')")
+    for tuple in cur.fetchall():
+        print(tuple)
+    cur.close()
+    con.close
