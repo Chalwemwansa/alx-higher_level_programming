fe98c70e44c07db5f2eaaee889afabe2b76742bd
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 01:52:57 +0200

    safe from sql injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""a script that only executes when is is not imported and
+    retrieves values from a database according to values passed as an argument
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """section where the connection and execution of queries takes place from
+    """
+    querry = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY\
+             id ASC"
+    con = MySQLdb.connect(host="localhost", port=3306, passwd=argv[2],
+                          user=argv[1], db=argv[3], charset='utf8')
+    cur = con.cursor()
+    cur.execute(querry, (argv[4],))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    con.close()
