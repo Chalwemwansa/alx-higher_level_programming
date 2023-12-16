a4119f185a84fbea0fc96cd001c5baab95aa2e77
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 16:52:14 +0200

    checking last mandatory task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""prints all the city objects from a given database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine, asc, label
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/\
+{argv[3]}", pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State.name.label('state_name'), City.id, City.name)
+
+    rst = query.filter(City.state_id == State.id).order_by(asc(City.id)).all()
+    for row in rst:
+        print(f"{row.state_name}: ({row.id}) {row.name}")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""script contains a class that maps to the cities database
+"""
+from model_state import Base
+from sqlalchemy import Integer, String, Column, ForeignKey
+
+
+class City(Base):
+    """class that maps to the cities database from the local machine
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
