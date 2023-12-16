16f6d1271ba25b71ce80e4c2fbe6aa345c8f0760
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 15:01:21 +0200

    checking tas 10 if it is ok

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""script that prints the names of the cities passed as an argument
+    to the python script
+"""
+from sys import argv
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/\
+{argv[3]}", pool_pre_ping=True)
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State)
+
+    result = query.filter(State.name == argv[4]).first()
+    if result is not None:
+        print(result.id)
+    else:
+        print("Not found")
