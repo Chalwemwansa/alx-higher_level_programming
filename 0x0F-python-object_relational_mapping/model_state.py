dd8202c702320141949543ca78e42bb1be6fb0e9
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 10:48:56 +0200

    task 6 checking

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""link class for states table, this module has to be imported to
+    be used to map a relational database to python objects
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """the class that will be used to link to a relational database
+    from python
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
