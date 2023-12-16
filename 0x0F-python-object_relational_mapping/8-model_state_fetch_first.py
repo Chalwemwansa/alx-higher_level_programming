4650fc8d8cfe12e325ce8caaeaa11d14b2c6402f
chalwemwansa <chalwemwansa23@gmail.com>
2023-12-16 13:39:36 +0200

    checking task8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""script that prints the first State from the database given
+    module can not be imported but can import other modules
+"""
+from sys import argv
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/\
+{argv[3]}", pool_pre_ping=True)
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State)
+    result = query.order_by(asc(State.id)).limit(1)
+    print(f"{result[0].id}: {result[0].name}")
