30d7b4550e30cd62976a8ab778da87c64c40a7e8
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-06 20:42:24 +0200

    worked on task10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-# this script 
+""" this script takes arguments and connects to the github api
+    then outputs the id of the user
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    passwd = argv[2]
+    url = f'https://api.github.com/users/{argv[1]}'
+    header = {'Authorization': f'Bearer {passwd}'}
+
+    request = requests.get(url, headers=header)
+    print(request.json().get('id'))
