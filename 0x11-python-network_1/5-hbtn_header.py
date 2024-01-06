9e402a1ce6203d976668ee62a87c2693e39200a3
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-06 18:14:37 +0200

    worked on task5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-# this script 
+""" this script takes in a url and sends a request to the url
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
