dac9737791985251b75a6ee78f6d06b58a00cd64
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-06 18:20:30 +0200

    worked on task6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-# this script 
+""" this script takes in an email address and sends
+    a POST request to the given server
+"""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    request = requests.post(argv[1], data={'email': argv[2]})
+    print(request.text)
