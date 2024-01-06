cd7cdba83191509b7c9a926f6214c03474e2e0a1
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-06 16:47:41 +0200

    worked on task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-# this script 
+""" script that fetches data from a given url
+    and prints the value of a given variable
+"""
+import urllib.request
+from sys import argv
+
+request = urllib.request.Request(argv[1])
+with urllib.request.urlopen(request) as request_data:
+    r = request_data.headers
+print(r['X-Request-Id'])
