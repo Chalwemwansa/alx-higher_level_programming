a4f4b4ab06e978094eb40976befe88f4b23c118f
chalwemwansa <chalwemwansa23@gmail.com>
2024-01-06 18:30:58 +0200

    worked on task7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-# this script 
+""" this script takes in a url and sends a request to the url
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    request = requests.get(argv[1])
+    if request.status_code >= 400:
+        print(f"Error code: {request.status_code}")
+    else:
+        print(request.decode('utf8'))
